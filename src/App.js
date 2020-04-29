2355ff9e4ecf6e1ffba1f93ea6b4be18c30e96d7
pratham jain <prathamjn2000@gmail.com>
2020-04-29 20:10:55 +0530

    configuring react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
+import {Navbar, NavbarBrand} from 'reactstrap'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React App
-        </a>
-      </header>
+      <Navbar dark color="primary">
+       <div className="container">
+         <NavbarBrand href="/">Con Fusion</NavbarBrand>
+       </div>
+      </Navbar>
+
     </div>
   );
 }

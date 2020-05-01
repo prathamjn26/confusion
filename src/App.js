7fa651cbfd7eae7d04096241a1d5d8c3a06701ee
pratham jain <prathamjn2000@gmail.com>
2020-05-01 18:39:02 +0530

    card add in component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Navbar, NavbarBrand} from 'reactstrap'
 import './App.css';
 import { MenuComponent } from './components/MenuComponent';
+import {DISHES} from './shared/dishes'
 
-function App() {
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       dishes:DISHES
+    }
+  }
+  
+
+  render()
+  {
   return (
     <div>
       <Navbar dark color="primary">
@@ -11,9 +24,10 @@ function App() {
          <NavbarBrand href="/">Con Fusion</NavbarBrand>
        </div>
       </Navbar>
-    <MenuComponent/>
+    <MenuComponent dishes={this.state.dishes}/>
     </div>
   );
+ }
 }
 
 export default App;

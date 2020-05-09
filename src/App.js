052a85a5056abd5a4a9f5ec5db5a72325f90ac4c
pratham jain <prathamjn2000@gmail.com>
2020-05-09 12:59:50 +0530

    Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 import './App.css';
 import Main from './components/MainComponent';
 
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import {Navbar, NavbarBrand} from 'reactstrap'
 import Menu from './MenuComponent';
 import {DISHES} from '../shared/dishes'
 import DishDetail from './DishdetailComponent'
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 
 class Main extends Component {
 
@@ -17,7 +18,6 @@ class Main extends Component {
   
   onDishSelect(DishId)
   {
-      console.log(DishId)
       this.setState({
         selectedDish:DishId
       })
@@ -27,13 +27,10 @@ class Main extends Component {
   {
   return (
     <div>
-      <Navbar dark color="primary">
-       <div className="container">
-         <NavbarBrand href="/">Con Fusion</NavbarBrand>
-       </div>
-      </Navbar>
+      <Header/>
       <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
       <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+      <Footer/>
     </div>
   );
  }
